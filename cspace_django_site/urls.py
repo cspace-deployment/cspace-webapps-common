6f9c75d17fea908f27c53638edf02cbf70a4c8a8
John Lowe <jblowe@berkeley.edu>
2019-06-28 17:52:23 -0700

    CSW-77: revisions to basic django project settings

--- cspace_django_site/urls.py
+++ cspace_django_site/urls.py
@@ -14,8 +14,34 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+#from django.urls import path, include
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+from django.contrib.auth import views
+
+# admin.autodiscover()
+
+#
+# Initialize our web site -things like our AuthN backend need to be initialized.
+#
+from cspace_django_site.main import cspace_django_site
+
+cspace_django_site.initialize()
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #  Examples:
+    #  url(r'^$', 'cspace_django_site.views.home', name='home'),
+    #  url(r'^cspace_django_site/', include('cspace_django_site.foo.urls')),
+
+    #  Uncomment the admin/doc line below to enable admin documentation:
+    #  url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # url(r'^$', 'landing.views.index', name='index'),
+    # these are django builtin webapps
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.LoginView, name='login'),
+    url(r'^accounts/logout/$', views.LogoutView, name='logout')
 ]
