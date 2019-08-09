f9f5bb254f06643e0eb34f48360ccb94747d9c4b
John Lowe <jblowe@berkeley.edu>
2019-08-09 09:57:54 -0700

    CSW-99: set default log level to 'WARNING'; add dir to template directories

--- cspace_django_site/settings.py
+++ cspace_django_site/settings.py
@@ -133,7 +133,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'handlers': {
         'file': {
-            'level': 'DEBUG',
+            'level': 'WARNING',
             'class': 'logging.FileHandler',
             'filename': os.path.join(BASE_DIR, 'logs', 'logging.txt'),
         },
