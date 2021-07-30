fa7f7243be6b0a1e6e312622a7148f67c92585cb
John Lowe <jblowe@berkeley.edu>
2021-07-30 10:52:32 -0700

    NOJIRA: improve 'check UCJEPS images' script

--- uploadmedia/checkUCJEPSmedia.py
+++ uploadmedia/checkUCJEPSmedia.py
@@ -32,13 +32,13 @@ def getRecords(rawFile, key):
 
 if __name__ == "__main__":
 
-    if len(sys.argv) == 4:
+    if len(sys.argv) == 5:
         print("MEDIA: input file 1 (fully qualified path): %s" % sys.argv[1])
         print("MEDIA: input file 2 (fully qualified path): %s" % sys.argv[2])
     else:
         print()
-        print("usage: %s <findcommandresult> <cspacemedia> <outputfile>" % sys.argv[0])
-        print("e.g.   %s findcommandresult.csv cspacemedia.cfg outputfile.cfg" % sys.argv[0])
+        print("usage: %s <findcommandresult> <cspacemedia> <cspaceaccessions> <outputfile>" % sys.argv[0])
+        print("e.g.   %s findcommandresult.txt cspacemedia.csv cspaceaccessions.csv outputfile.csv" % sys.argv[0])
         print()
         sys.exit(1)
 
@@ -49,11 +49,11 @@ if __name__ == "__main__":
         sys.exit()
     print('MEDIA: %s columns and %s lines found in file %s' % (columns1, len(records1), sys.argv[1]))
     stats[f'input: {sys.argv[1]}'] = len(records1)
+
     records2, columns2 = getRecords(sys.argv[2], 1)
     if columns2 == -1:
         print('MEDIA: Error! %s' % sys.argv[2])
         sys.exit()
-
     errors = 0
     media_info = {}
     for r in records2:
@@ -65,11 +65,26 @@ if __name__ == "__main__":
     print('MEDIA: %s columns and %s lines found in file %s' % (columns2, len(records2), sys.argv[2]))
     stats[f'input: {sys.argv[2]}'] = len(records2)
 
-    outputfh = csv.writer(open(sys.argv[3], 'w'), delimiter="|")
+    records3, columns3 = getRecords(sys.argv[3], 1)
+    if columns3 == -1:
+        print('MEDIA: Error! %s' % sys.argv[3])
+        sys.exit()
+    errors = 0
+    accession_info = {}
+    for r in records3:
+        if len(r) == 2:
+            accession_info[r[1]] = r
+        else:
+            print(r)
+            errors += 1
+    print('MEDIA: %s columns and %s lines found in file %s' % (columns3, len(records3), sys.argv[3]))
+    stats[f'input: {sys.argv[3]}'] = len(records3)
+
+    outputfh = csv.writer(open(sys.argv[4], 'w'), delimiter="|")
     # the first row of the file is a header
     columns = records2[0]
     del records2[0]
-    outputfh.writerow(['filepath'] + columns)
+    outputfh.writerow(['filepath', 'image found', 'accession found'] + columns)
 
     filenames = {}
     for i, r in enumerate(records1):
@@ -77,18 +92,26 @@ if __name__ == "__main__":
         fn = f.split('/')[-1]
         if '.JPG' in f or '.CR2' in f:
             fn = fn.replace('.JPG','').replace('.CR2','')
+            fn = fn.split('_')[0]
             filenames[fn] = f
     stats[f'filenames'] = len(filenames)
 
-    stats['found in cspace'] = 0
-    stats['not in cspace'] = 0
+    stats['image found in cspace'] = 0
+    stats['image not in cspace'] = 0
+    stats['accession exists'] = 0
+    stats['accession not found'] = 0
     for f in filenames:
-        if f in media_info:
-            outputfh.writerow([filenames[f]] + ['found'] + media_info[f])
-            stats['found in cspace'] += 1
+        if f in accession_info:
+            accession_exists = 'accession exists'
         else:
-            outputfh.writerow([filenames[f]] + ['not in cspace'])
-            stats['not in cspace'] += 1
+            accession_exists = 'accession not found'
+        stats[accession_exists] += 1
+        if f in media_info:
+            outputfh.writerow([filenames[f]] + ['image found', accession_exists] + media_info[f])
+            stats['image found in cspace'] += 1
+        else:
+            outputfh.writerow([filenames[f]] + ['image not in cspace', accession_exists])
+            stats['image not in cspace'] += 1
 
     for s in sorted(stats):
         print(f'{s} {stats[s]}')
