ceccdcc12f295f2c2434595e2760c206efaf3bfd
John Lowe <jblowe@berkeley.edu>
2022-04-21 17:09:59 -0700

    CSW-609: improve script that update jrxml files for ireports webapp

--- ireports/getjrxml.sh
+++ ireports/getjrxml.sh
@@ -1,6 +1,6 @@
-rm -rf ~/services
+rm -rf ${HOME}/services
 git clone https://github.com/cspace-deployment/services.git
-cd ~/services
+cd ${HOME}/services
 git checkout origin/bampfa_5.1
 cp services/report/3rdparty/jasper-cs-report/src/main/resources/*.jrxml ~/jrxml
 git checkout origin/botgarden_5.1
@@ -11,5 +11,5 @@ git checkout origin/pahma_5.1
 cp services/report/3rdparty/jasper-cs-report/src/main/resources/*.jrxml ~/jrxml
 git checkout origin/ucjeps_5.1
 cp services/report/3rdparty/jasper-cs-report/src/main/resources/*.jrxml ~/jrxml
-cd ~
-rm -rf ~/services
+cd
+rm -rf ${HOME}/services
