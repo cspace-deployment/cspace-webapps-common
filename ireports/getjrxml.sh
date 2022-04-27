36b458ddd50d7672a52dbaf40b320b34caafa56d
John Lowe <jblowe@berkeley.edu>
2022-04-27 16:28:51 -0700

    NOJIRA: ireports helper app needs to look in correct locations

--- ireports/getjrxml.sh
+++ ireports/getjrxml.sh
@@ -2,10 +2,9 @@ rm -rf ${HOME}/services
 git clone https://github.com/cspace-deployment/services.git
 cd ${HOME}/services
 git checkout origin/ucb_6.0
+# copy 'core' reports
 cp services/report/3rdparty/jasper-cs-report/src/main/resources/*.jrxml ~/jrxml
-cp services/report/3rdparty/jasper-cs-report/src/main/resources/*.jrxml ~/jrxml
-cp services/report/3rdparty/jasper-cs-report/src/main/resources/*.jrxml ~/jrxml
-cp services/report/3rdparty/jasper-cs-report/src/main/resources/*.jrxml ~/jrxml
-cp services/report/3rdparty/jasper-cs-report/src/main/resources/*.jrxml ~/jrxml
+# copy reports for all tenants
+cp services/report/3rdparty/jasper-cs-report/src/main/resources/tenants/*/*.jrxml ~/jrxml
 cd
 rm -rf ${HOME}/services
