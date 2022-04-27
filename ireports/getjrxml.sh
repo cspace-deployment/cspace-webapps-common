084e8b3666e5fdb02470dd39266f193d6f85f864
John Lowe <jblowe@berkeley.edu>
2022-04-27 16:20:51 -0700

    NOJIRA: ireports helper app needs only look in one location for .jrxml

--- ireports/getjrxml.sh
+++ ireports/getjrxml.sh
@@ -1,15 +1,11 @@
 rm -rf ${HOME}/services
 git clone https://github.com/cspace-deployment/services.git
 cd ${HOME}/services
-git checkout origin/bampfa_5.1
+git checkout origin/ucb_6.0
 cp services/report/3rdparty/jasper-cs-report/src/main/resources/*.jrxml ~/jrxml
-git checkout origin/botgarden_5.1
 cp services/report/3rdparty/jasper-cs-report/src/main/resources/*.jrxml ~/jrxml
-git checkout origin/cinefiles_5.1
 cp services/report/3rdparty/jasper-cs-report/src/main/resources/*.jrxml ~/jrxml
-git checkout origin/pahma_5.1
 cp services/report/3rdparty/jasper-cs-report/src/main/resources/*.jrxml ~/jrxml
-git checkout origin/ucjeps_5.1
 cp services/report/3rdparty/jasper-cs-report/src/main/resources/*.jrxml ~/jrxml
 cd
 rm -rf ${HOME}/services
