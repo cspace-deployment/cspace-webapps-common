46c92bd8b9e6896831de3be6e9b5dabb3dcd3cd0
John Lowe <jblowe@berkeley.edu>
2022-05-23 09:25:38 -0700

    CSW-628: more bulletproofing of bmu s3 helper script

--- uploadmedia/cps3.sh
+++ uploadmedia/cps3.sh
@@ -6,15 +6,19 @@ if [[ $# -ne 3 ]] ; then
 fi
 
 BL_ENVIRONMENT=`/usr/bin/curl -s -m 5 http://169.254.169.254/latest/meta-data/tags/instance/BL_ENVIRONMENT`
+if [[ -z $BL_ENVIRONMENT || ( "$BL_ENVIRONMENT" != "dev" && "$BL_ENVIRONMENT" != "qa" && "$BL_ENVIRONMENT" != "prod" ) ]]; then
+	echo "Could not fetch BL_ENVIRONMENT, are you sure you're on AWS?"
+	exit 1
+fi
 BL_ENVIRONMENT="blacklight-${BL_ENVIRONMENT}"
 
 if [[ "$3" == "to" ]] ; then
-  /usr/bin/aws s3 cp /tmp/$1 s3://${BL_ENVIRONMENT}/bmu/$2/$1
-  echo "/usr/bin/aws s3 cp /tmp/$1 s3://${BL_ENVIRONMENT}/bmu/$2/$1"
-  rm /tmp/$1
+  /usr/bin/aws s3 cp "/tmp/$1" s3://${BL_ENVIRONMENT}/bmu/$2/$1
+  echo "/usr/bin/aws s3 cp '/tmp/$1' s3://${BL_ENVIRONMENT}/bmu/$2/$1"
+  rm "/tmp/$1"
 elif [[ "$3" == "from" ]] ; then
-  /usr/bin/aws s3 cp s3://${BL_ENVIRONMENT}/bmu/$2/$1 /tmp
-  echo "/usr/bin/aws s3 cp s3://${BL_ENVIRONMENT}/bmu/$2/$1 /tmp"
+  /usr/bin/aws s3 cp "s3://${BL_ENVIRONMENT}/bmu/$2/$1" /tmp
+  echo "/usr/bin/aws s3 cp 's3://${BL_ENVIRONMENT}/bmu/$2/$1' /tmp"
 else
   echo "direction must be either from or to"
   exit 1
