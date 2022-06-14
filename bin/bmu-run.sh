904aad86fee81ef215e5ffe3e1905d8f368d836e
John Lowe <jblowe@berkeley.edu>
2022-06-14 09:25:18 -0700

    CSW-634: minor refactoring of new 'bmu-run.py' script

--- bin/bmu-run.sh
+++ bin/bmu-run.sh
@@ -3,6 +3,8 @@
 source ${HOME}/pipeline-config.sh
 
 for t in bampfa botgarden cinefiles pahma ucjeps; do
-  bash -l -c "shopt -s nullglob ; for f in /cspace/bmu/${t}*.step1.csv; do f=$(echo $f | sed -e "s/\.step1.csv//") ; { time /var/www/${t}/uploadmedia/postblob.sh ${t} $f uploadmedia_batch ; } >> /cspace/bmu/${t}/batches.log 2>&1 ; done"
-  ${HOME}/bin/cleanBMUtempdir.sh ${HOMEDIR}/bmu/${t} >> ${HOMEDIR}/monitor/${t}.imagedircleanup.log
+  shopt -s nullglob ; for f in /cspace/bmu/${t}/*.step1.csv
+  do f=$(echo $f | sed -e "s/\.step1.csv//")
+    time /var/www/${t}/uploadmedia/postblob.sh ${t} $f uploadmedia_batch  >> /cspace/bmu/${t}/batches.log 2>&1
+  done
 done
