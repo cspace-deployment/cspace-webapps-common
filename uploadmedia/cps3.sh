90c370a2c5f99d698f1247ce057ee37a43c44bd8
John Lowe <jblowe@berkeley.edu>
2022-06-22 13:00:37 -0700

    CSW-702: eliminate superfluous message from s3 copy from bmu log

--- uploadmedia/cps3.sh
+++ uploadmedia/cps3.sh
@@ -13,11 +13,11 @@ fi
 BL_ENVIRONMENT="blacklight-${BL_ENVIRONMENT}"
 
 if [[ "$3" == "to" ]] ; then
-  /usr/bin/aws s3 cp "/tmp/$1" s3://${BL_ENVIRONMENT}/bmu/$2/$1
+  /usr/bin/aws s3 cp --quiet "/tmp/$1" s3://${BL_ENVIRONMENT}/bmu/$2/$1
   echo "/usr/bin/aws s3 cp '/tmp/$1' s3://${BL_ENVIRONMENT}/bmu/$2/$1"
   rm "/tmp/$1"
 elif [[ "$3" == "from" ]] ; then
-  /usr/bin/aws s3 cp "s3://${BL_ENVIRONMENT}/bmu/$2/$1" /tmp
+  /usr/bin/aws s3 cp --quiet "s3://${BL_ENVIRONMENT}/bmu/$2/$1" /tmp
   echo "/usr/bin/aws s3 cp 's3://${BL_ENVIRONMENT}/bmu/$2/$1' /tmp"
 else
   echo "direction must be either from or to"
