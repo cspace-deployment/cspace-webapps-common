a79c7f93359506947cb6c9c27ddf6f15d3d2b9bd
John Lowe <jblowe@berkeley.edu>
2022-10-05 19:05:59 -0700

    CSW-743: update settings.py for secrets hack

--- cspace_django_site/settings.py
+++ cspace_django_site/settings.py
@@ -19,11 +19,12 @@ except ImportError:
 from cspace_django_site.installed_apps import INSTALLED_APPS
 from cspace_django_site.webapps_global_config import *
 
+# TODO: this hack to be repaired soon
 # generate a secret each time
-SETTINGS_DIR = os.path.abspath(os.path.dirname(__file__))
-from cspace_django_site.secret_key_gen import *
-generate_secret_key(os.path.join(SETTINGS_DIR, 'secret_key.py'))
-from cspace_django_site.secret_key import *
+# SETTINGS_DIR = os.path.abspath(os.path.dirname(__file__))
+# from cspace_django_site.secret_key_gen import *
+# generate_secret_key(os.path.join(SETTINGS_DIR, 'secret_key.py'))
+# from cspace_django_site.secret_key import *
 
 # this is set in the various "extra_*.py" files
 # ALLOWED_HOSTS = []
