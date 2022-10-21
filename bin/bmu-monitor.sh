d49b0a684ef01622f43ffc85d45fa1417f8e495a
John Lowe <jblowe@berkeley.edu>
2022-10-21 07:01:42 -0700

    CSW-711: minor refactoring of bmu-invoking cron scripts; remove rjaffe as nightly report recipient

--- bin/bmu-monitor.sh
+++ bin/bmu-monitor.sh
@@ -3,7 +3,7 @@
 source ${HOME}/pipeline-config.sh
 
 # NB: BAM no longer uses the BMU: derivatives from Piction are pushed to CSpace
-for t in botgarden cinefiles pahma ucjeps; do python3 /var/www/$t/uploadmedia/checkRuns.py ${HOMEDIR}/bmu/$t jobs summary $t | mail -r "cspace-support@lists.berkeley.edu" -s "recent $t BMU jobs" jblowe@berkeley.edu,rjaffe@berkeley.edu; done
+for t in botgarden cinefiles pahma ucjeps; do python3 /var/www/$t/uploadmedia/checkRuns.py ${HOMEDIR}/bmu/$t jobs summary $t | mail -r "cspace-support@lists.berkeley.edu" -s "recent $t BMU jobs" jblowe@berkeley.edu; done
 for t in botgarden cinefiles pahma ucjeps; do python3 /var/www/$t/uploadmedia/checkRuns.py ${HOMEDIR}/bmu/$t jobs summary $t > /var/www/static/${t}.nightly.report.txt ; done
 # bmu 'usage graphs': plots of nightly bmu uploads
 bash -l -c 'python3 /var/www/pahma/uploadmedia/bmu-nightly-2022-concise.py /cspace/monitor > /dev/null'
--- bin/bmu-run.sh
+++ bin/bmu-run.sh
@@ -5,6 +5,6 @@ source ${HOME}/pipeline-config.sh
 for t in bampfa botgarden cinefiles pahma ucjeps; do
   shopt -s nullglob ; for f in /cspace/bmu/${t}/*.step1.csv
   do f=$(echo $f | sed -e "s/\.step1.csv//")
-    time /var/www/${t}/uploadmedia/postblob.sh ${t} $f uploadmedia_batch  >> /cspace/bmu/${t}/batches.log 2>&1
+    time /var/www/${t}/uploadmedia/postblob.sh ${t} ${f} uploadmedia_batch  >> /cspace/bmu/${t}/batches.log 2>&1
   done
 done
