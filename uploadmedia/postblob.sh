26ed8fc382aa225de4dbd6366a80999ef3e914c6
John Lowe <jblowe@berkeley.edu>
2022-11-17 06:53:38 -0800

    CSW-712: back out cr2 handling from generic bmu; they can ask for it if they need it

--- uploadmedia/postblob.sh
+++ uploadmedia/postblob.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# these better be here!
-source ~/.profile
+# this better be here!
 source ${HOME}/venv/bin/activate
 
 # three arguments required:
@@ -54,40 +53,11 @@ else
     trace "input file: $INPUTFILE"
 fi
 trace ">>>>>>>>>>>>>>> Starting Blob, Media, and Relation record creation process: `date` "
-# handle .CR2 files: convert them to TIFs and JPGs, and upload then upload the JPGs 'as usual'
-# save the TIFs back into s3 for ingestion into Merritt
-# first, get a list of all the CR2s in the job
-grep -i '\.CR2' $INPUTFILE | cut -f1 -d"|" > CR2file
-# convert them all
-for CR2 in `cat CR2file`
-  do
-    F=$(echo "$CR2" | sed "s/\.CR2/_CR2/i")
-    # fetch the CR2 from S3
-    echo "${RUNDIR}/cps3.sh \"$CR2\" ucjeps from" >> $TRACELOG
-    ${RUNDIR}/cps3.sh "$CR2" ucjeps from >> $TRACELOG 2>&1
-    # make a jpg and a tif for each cr2
-    for FORMAT in JPG TIF
-    do
-      echo "converting ${CR2} to ${F}.${FORMAT}" >> $TRACELOG
-      convert -verbose "/tmp/${CR2}" "/tmp/${F}.${FORMAT}" >> $TRACELOG 2>&1
-      # put the converted file back into S3
-      echo "${RUNDIR}/cps3.sh \"${F}.${FORMAT}\" ucjeps to" >> $TRACELOG
-      ${RUNDIR}/cps3.sh "${F}.${FORMAT}" ucjeps to >> $TRACELOG 2>&1
-    done
-  rm "/tmp/${CR2}"
-done
-# change the file names in the bmu job file so that it will upload the JPGs
-perl -i -pe 's/\.CR2/_CR2.JPG/i' $INPUTFILE
-# make a copy of the input file suitable for input to the merritt archiving system
-perl -pe 's/\.CR2/_CR2.TIF/i' $INPUTFILE > $JOB.archive.csv
-
-trace "python $UPLOADSCRIPT $INPUTFILE $MEDIACONFIG >> $TRACELOG"
-python $UPLOADSCRIPT $INPUTFILE $MEDIACONFIG >> $TRACELOG 2>&1
+trace "python3 $UPLOADSCRIPT $INPUTFILE $MEDIACONFIG >> $TRACELOG"
+python3 $UPLOADSCRIPT $INPUTFILE $MEDIACONFIG >> $TRACELOG 2>&1
 trace "Media record and relations created."
 
 mv $INPUTFILE $JOB.original.csv
 mv $JOB.step3.csv $JOB.processed.csv
-
-rm CR2file
 
 trace "**** END OF RUN ******************** `date` **************************"
