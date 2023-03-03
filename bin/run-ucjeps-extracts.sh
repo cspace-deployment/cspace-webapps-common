900f42754054e16a0b41df29ec60a1007f004832
John Lowe <jblowe@berkeley.edu>
2023-03-03 12:09:07 -0800

    CSW-812: run-ucjeps-extracts.sh 'soft-delete' script needs parameter

--- bin/run-ucjeps-extracts.sh
+++ bin/run-ucjeps-extracts.sh
@@ -8,5 +8,5 @@ ${HOME}/bin/major_group.sh > /dev/null 2>&1
 ${HOME}/bin/get_taxonauth.sh taxon > /dev/null 2>&1
 ${HOME}/bin/get_taxonauth.sh unverified > /dev/null 2>&1
 ${HOME}/bin/extract_authorities_nightly.sh > /dev/null 2>&1
-${HOME}/bin/extract-soft-deletes.sh > /dev/null 2>&1
+${HOME}/bin/extract-soft-deletes.sh ucjeps > /dev/null 2>&1
 foo=`${HOME}/bin/ucjepsextracts2s3.sh 2>&1` ;  echo "$foo" | mail -r "cspace-support@lists.berkeley.edu" -s "extracts sent to s3" ${UCJEPS_CONTACT}
