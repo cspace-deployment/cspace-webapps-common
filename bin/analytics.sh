7a04eca2c07bcab3a786789b2d56b5806e8389e2
John Lowe <jblowe@berkeley.edu>
2023-03-29 16:48:51 -0700

    CSW-819: update contact email for web analytics

--- bin/analytics.sh
+++ bin/analytics.sh
@@ -4,7 +4,7 @@
 
 source ${HOME}/pipeline-config.sh
 
-/cspace/monitor/checklogs.sh | mail -r "cspace-support@lists.berkeley.edu" -s "Access Log Analysis" jblowe@berkeley.edu
+/cspace/monitor/checklogs.sh | mail -r "cspace-support@lists.berkeley.edu" -s "Access Log Analysis" ${TECH_LEAD}
 # analyze and summarize the django logs and solr cores, keep a copy of an extract of the other vhost logs
 cd /cspace/monitor ; ./analyze.sh >> analytics.log  2>&1 ; ./corestats.sh >> analytics.log 2>&1
 wc -l /cspace/bmu/*/*.processed.csv | perl -pe 's/^ *(\d+) /\1\t/;s#/cspace/bmu/(.*)#\1#;s#/#\t#g;s/.processed.csv//' > /cspace/monitor/bmu-counts.csv
