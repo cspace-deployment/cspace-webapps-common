2cfbc2ae1d5873b277ddf10d37afa131799fe7e9
John Lowe <jblowe@berkeley.edu>
2023-05-01 10:15:34 -0700

    CSW-844: refactor and improve bin/analytics.sh to reduce temp space used, etc.

--- bin/analytics.sh
+++ bin/analytics.sh
@@ -4,10 +4,20 @@
 
 source ${HOME}/pipeline-config.sh
 
-/cspace/monitor/checklogs.sh | mail -r "cspace-support@lists.berkeley.edu" -s "Access Log Analysis" ${TECH_LEAD}
+YEAR=`date +%Y`
+MONTH=`date +%m`
+
+cd /cspace/monitor
+./checklogs.sh | mail -r "cspace-support@lists.berkeley.edu" -s "Access Log Analysis" ${TECH_LEAD}
 # analyze and summarize the django logs and solr cores, keep a copy of an extract of the other vhost logs
-cd /cspace/monitor ; ./analyze.sh >> analytics.log  2>&1 ; ./corestats.sh >> analytics.log 2>&1
+./analyze.sh >> analytics.log  2>&1 ; ./corestats.sh >> analytics.log 2>&1
+# count media uploaded via bmu
 wc -l /cspace/bmu/*/*.processed.csv | perl -pe 's/^ *(\d+) /\1\t/;s#/cspace/bmu/(.*)#\1#;s#/#\t#g;s/.processed.csv//' > /cspace/monitor/bmu-counts.csv
+# count ip addresses in logs
+cat /var/log/apache2/*/*/*.log | cut -f1 -d" " | sort | uniq -c | sort -rn | head -50 > /cspace/monitor/${YEAR}-{MONTH}-ips.txt
+# compute some imageserver stats
+./extract-imageserver-gets.sh > /var/www/static/imageserver-stats.html
 # recompute goaccess stats daily
 ${HOME}/bin/goaccess.sh
+# make a list of all the metrics for easy reference
 ${HOME}/bin/make-metrics.sh
--- bin/monitor/extract-imageserver-gets.sh
+++ bin/monitor/extract-imageserver-gets.sh
@@ -1,11 +1,36 @@
+#!/bin/bash
+
+# compute some usages statistics for the image server
+
+YEAR=`date +%Y`
+MONTH=`date +%m`
+
 grep imageserver.image /cspace/monitor/*.django.log | cut -f2- -d":" | cut -f1-3,7,8 | perl -pe 's/ seconds//;s#blobs/##;s/ +//g;s/\t\t/\t/g;' > is.extract.csv
 perl -pe 's#/derivatives/(.*?)/content#\t\1#;s#/content#\tOriginal#' is.extract.csv > i2.csv
 rm is.extract.csv
 perl -ne 'print if /\t(Thumbnail|Medium|Original|Large|OriginalJpeg)\t/' i2.csv > imageserver.gets.csv &
 perl -ne 'print unless /\t(Thumbnail|Medium|Original|Large|OriginalJpeg)\t/' i2.csv > imageserver.errors.csv &
 wait
-cut -f5 imageserver.gets.csv | sort | uniq -c &
-wc -l imageserver.errors.csv
-wait
 rm i2.csv
-
+echo "<html>"
+echo "<h3>Some ImageServer Usage Statistics"
+echo "<h4>ImageServer gets by derivative type, since 2020</h4><pre>"
+cut -f5 imageserver.gets.csv | sort | uniq -c
+echo
+echo "</pre><h4>ImageServer gets by museum, since 2020</h4><pre>"
+cut -f1 imageserver.gets.csv | sort | uniq -c
+echo
+echo "</pre><h4>ImageServer gets by derivative, this month, ${YEAR}-${MONTH}</h4><pre>"
+grep "${YEAR}-${MONTH}-" imageserver.gets.csv | cut -f5 | sort | uniq -c
+echo
+echo "</pre><h4>ImageServer gets by museum, this month, ${YEAR}-${MONTH}</h4><pre>"
+grep "${YEAR}-${MONTH}-" imageserver.gets.csv | cut -f1 | sort | uniq -c
+echo
+echo "</pre><h4>ImageServer gets by month for the last year</h4><pre>"
+cut -f2 imageserver.gets.csv | cut -c1-7 | sort -r | uniq -c | head -12
+echo
+echo "</pre><h4>ImageServer request errors</h4><pre>"
+wc -l imageserver.errors.csv
+echo "</pre></html>"
+gzip imageserver.gets.csv
+rm imageserver.errors.csv
