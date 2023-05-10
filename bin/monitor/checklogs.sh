0fbb864c0d0c0a54b5eb589945ff700270aef55d
John Lowe <jblowe@berkeley.edu>
2023-05-10 14:15:08 -0700

    CSW-844: checklogs.sh now checks for 403s

--- bin/monitor/checklogs.sh
+++ bin/monitor/checklogs.sh
@@ -6,9 +6,16 @@ echo "Q&D access log analysis"
 echo
 echo "500s:"
 echo
-grep " 500 " $LOGS | grep -v -i bot | grep -v imageserver | perl -pe 's/^.*?://;s/\" \".*/"/' |\
- sort -t ' ' -k 4.9,4.12n -k 4.5,4.7M -k 4.2,4.3n -k 4.14,4.15n -k 4.17,4.18n -k 4.20,4.21n | tac | head -50
+grep " 500 " $LOGS | grep -v -i bot | grep -v imageserver | perl -pe 's/^.*?\[//;s/ .*?\"/ "/;s/ "\-" .*//' |\
+ sort -t ' ' -k 4.9,4.12n -k 4.5,4.7M -k 4.2,4.3n -k 4.14,4.15n -k 4.17,4.18n -k 4.20,4.21n | tac | head -20
 echo
+
+echo "403:"
+echo
+grep " 403 " $LOGS | grep -v -i bot | grep -v imageserver | perl -pe 's/^.*?\[//;s/ .*?\"/ "/;s/ "\-" .*//' |\
+ sort -t ' ' -k 4.9,4.12n -k 4.5,4.7M -k 4.2,4.3n -k 4.14,4.15n -k 4.17,4.18n -k 4.20,4.21n | tac | head -20
+echo
+
 echo "Dates:"
 echo
 cut -f3 -d"-" $LOGS | cut -f1 -d ":" | perl -pe 's/ \[//' | sort -t ' ' -k 1.8,1.11n -k 1.4,1.6M -k 1.1,1.2n |\
